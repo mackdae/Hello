86b353ed5cc1ade5a131ca9e35d7124204de3a05
mackdae <millim0821@naver.com>
2023-04-21 16:17:02 +0900

    초기 커밋.

---
+++ Hello/MainApp.cs
@@ -0,0 +1,25 @@
+﻿using System; // using키워드 // System(네임스페이스) 사용
+using static System.Console; // System의 Console(클래스) 사용
+// 상단에 참조를 미리 써두면 하단에 경로 생략 가능
+/* 문단주석
+(중첩시 에러) */
+
+namespace Hello
+{
+    class MainApp
+    {
+        static void Main(string[] args) //string배열타입 사용자 입력값 받음
+        // Main 프로그램의 진입점 (실행 시작점)
+        // 모든 프로그램은 Main 메소드를 하나 포함해야 함 // 메소드 = 함수
+        // static한정자 void반환형식 Main메소드명 ()매개변수 {}코드블록
+        {
+            if (args.Length == 0) //만약 입력값이 없으면
+            {
+                Console.WriteLine("사용법 : Hello.exe <이름>"); //사용법 출력
+                return; //메소드 종료
+            }
+
+            WriteLine("Hello, {0}!", args[0]); //사용자 입력값을 출력
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello, C# World!");
+            WriteLine("");
+            WriteLine("여러분, 안녕하세요?");
+            WriteLine("반갑습니다.");
+            WriteLine("");
+            WriteLine("C# 어렵지 않아요~");
+            WriteLine("우리 같이!");
+            WriteLine("익숙해져봅시다~");
+
+            int x; //변수선언
+            x = 100; //데이터할당 //초기화: 최초의 데이터할당
+            int y = 100;
+            //int데이터형식, y식별자(변수명), =대입연산자, 100데이터(값), ;종료
+
+            // 값형식 - 스택 메모리 - short, int, long, ..., 구조체(Struct)
+            // 참조형식 - 힙 메모리 - 복합 데이터 형식, 클래스(Class)
+        }
+    }
+}
